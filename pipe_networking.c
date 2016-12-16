5b243595b3435a946451d632f7726575e23301d7
sabiden@stuy.edu <sabiden@stuy.edu>
2016-12-16 15:39:35 -0500

    Updated

--- pipe_networking.c
+++ pipe_networking.c
@@ -2,27 +2,27 @@
 
 int client_handshake(int* x){
   mkfifo("25645", 0644);
-  printf("[CLIENT]: PP made\n");
-  int fd = open("server", O_WRONLY);
-  write(fd,"25645", 6);
+  printf("[CLIENT]: 25645 Pipe made\n");
+  *x = open("WKP", O_WRONLY);
+  write(*x,"25645", 6);
   printf("[CLIENT]: Waiting for connection...\n");
-  fd = open("25645", O_RDONLY);
+  int pp = open("25645", O_RDONLY);
   char buffer[MESSAGE_BUFFER_SIZE];
-  read(fd, buffer, strlen(buffer));
-  printf("[CLIENT]: %s...\n", buffer);
-  close(fd);
-  
+  read(pp, buffer, strlen(buffer));
+  printf("[CLIENT]: Received: %s\n", buffer);
+  //remove
+  return pp; 
 }
+
 int server_handshake(int* x){
-  mkfifo("server", 0644);
+  mkfifo("WKP", 0644);
   printf("[SERVER]: WKP made\n");
   printf("[SERVER]: Waiting for connection...\n");
-  int fd = open("server", O_RDONLY);
-  char buffer[MESSAGE_BUFFER_SIZE];
-  read(fd, buffer, strlen(buffer));
-  printf("[SERVER]: Connection made with: %s", buffer);
-  close(fd);
-  fd = open(buffer, O_WRONLY);
-  char mssg[] = 'connected';
-  write(fd, mssg, strlen(mssg));
+  *x = open("WKP", O_RDONLY);
+  char pp[MESSAGE_BUFFER_SIZE];
+  read(*x, pp, strlen(pp));
+  printf("[SERVER]: Connection made with: %s\n", pp);
+  int fd = open(pp, O_WRONLY);
+  write(fd, "Connected", 10);
+  return fd;
 }
